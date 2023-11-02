36f869e6c65757c5c9b03f2f453e60c9d9678c88
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-11-02 05:13:00 -0600

    update

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// Variables
+var username = prompt("Enter your name")
+var age = prompt("What is your age?")
+var month = prompt("🤔Wait what was your birth month?🤔")
+
+// inital greeting
+alert("Howdy " + username + "! You are " + age + " years old!" + " and you were born in " + month);
+
+
+// Seasons and birthstones 
+if (month == "April" || "april" )
+{
+    alert("You were born in the spring and your birthstone is a diamond")
+}
+else if (month == "January" || "january")
+{
+    alert("You were born in the winter and your birthstone is a garnet")
+}
+else if (month == "Febuary" || "febuary")
+{
+    alert("You were born in the winter and your birthstone is a amethyst")
+}
+else if (month == "March" || "march")
+{
+    alert("You were born in the spring and your birthstone is a aquamarine")
+}
+else if (month == "May" || "may")
+{
+    alert("You were born in the spring and your birthstone is a emerald")
+}
+else if (month == "June" || "june")
+{
+    alert("You were born in the summer and your birthstone is a alexandrite")
+}
+else if (month == "July" || "july")
+{
+    alert("You were born in the summer and your birthstone is a ruby")
+}
+else if (month == "August" || "august")
+{
+    alert("You were born in the summer and your birthstone is a peridot")
+}
+else if (month == "September" || "september")
+{
+    alert("You were born in the fall and your birthstone is a sapphire")
+}
+else if (month == "October" || "october")
+{
+    alert("You were born during the spooky fall and your birthstone is a tourmaline")
+}
+else if (month == "November" || "november")
+{
+    alert("You were born in the fall and your birthstone is a citrine")
+}
+else if (month = "December" || "december")
+{
+    alert("You were born in the winter and your birthstone is a tanzanite")
+}
